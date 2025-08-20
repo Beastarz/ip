dba6f777732b6e446a625812531c24e9c2f22ffa
Beastarz <jygoh1453@gmail.com>
2025-08-20 10:39:28 +0800

    feat: Add echo function to repeat on what user input

--- src/main/java/Clare.java
+++ src/main/java/Clare.java
@@ -1,12 +1,39 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Clare {
-    public static void main(String[] args) {
-        String divider = "----------------------------------------";
-        String welcomeText = "Hello dear, I am Clare!\nWhat can I do for you?";
-        String farewellText = "Bye dear. Hope to see you again soon!";
+    static String divider = "----------------------------------------";
+
+    private static void welcome() {
+        String welcomeText = "Hello dear, I am Clare!\nSo happy to see you today.\nWhat can I help?";
         System.out.println(divider);
         System.out.println(welcomeText);
         System.out.println(divider);
+    }
+
+    private static void farewell() {
+        String farewellText = "Bye dear. I will miss you!";
+        System.out.println(divider);
         System.out.println(farewellText);
         System.out.println(divider);
+    }
+
+    private static void echo(String msg) {
+        System.out.println(divider);
+        System.out.println(msg);
+        System.out.println(divider);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        welcome();
+        String msg = scanner.next();
+        while (!Objects.equals(msg, "bye")) {
+            echo(msg);
+            msg = scanner.next();
+        }
+        farewell();
+
     }
 }
