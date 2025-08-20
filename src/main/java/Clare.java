dab64285dae4c5732157b2095c0943e1bcc39f1f
Beastarz <jygoh1453@gmail.com>
2025-08-20 10:59:38 +0800

    feat: Add add function and list function

--- src/main/java/Clare.java
+++ src/main/java/Clare.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Clare {
     static String divider = "----------------------------------------";
+    static ArrayList<String> tasks = new ArrayList<>();
 
     private static void welcome() {
         String welcomeText = "Hello dear, I am Clare!\nSo happy to see you today.\nWhat can I help?";
@@ -18,9 +20,18 @@ public class Clare {
         System.out.println(divider);
     }
 
-    private static void echo(String msg) {
+    private static void addList(String task) {
+        tasks.add(task);
         System.out.println(divider);
-        System.out.println(msg);
+        System.out.println("added: " + task);
+        System.out.println(divider);
+    }
+
+    private static void showList() {
+        System.out.println(divider);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i+1) + ". " + tasks.get(i));
+        }
         System.out.println(divider);
     }
 
@@ -28,10 +39,17 @@ public class Clare {
         Scanner scanner = new Scanner(System.in);
 
         welcome();
-        String msg = scanner.next();
+        String msg = scanner.nextLine();
         while (!Objects.equals(msg, "bye")) {
-            echo(msg);
-            msg = scanner.next();
+            switch (msg) {
+                case ("list"):
+                    showList();
+                    break;
+                default:
+                    addList(msg);
+                    break;
+            }
+            msg = scanner.nextLine();
         }
         farewell();
 
