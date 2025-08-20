e57151fba117a65572fbc5bf6f295e949e08a310
Beastarz <jygoh1453@gmail.com>
2025-08-20 10:11:47 +0800

    Rename file from Duke.java to Clare.java

---
+++ src/main/java/Clare.java
@@ -0,0 +1,12 @@
+public class Clare {
+    public static void main(String[] args) {
+        String divider = "----------------------------------------";
+        String welcomeText = "Hello dear, I am Clare!\nWhat can I do for you?";
+        String farewellText = "Bye dear. Hope to see you again soon!";
+        System.out.println(divider);
+        System.out.println(welcomeText);
+        System.out.println(divider);
+        System.out.println(farewellText);
+        System.out.println(divider);
+    }
+}
